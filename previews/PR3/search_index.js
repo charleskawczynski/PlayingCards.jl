7e9a9831cc63324de209477fd3afaffeb6f3b985
Documenter.jl <documenter@juliadocs.github.io>
2022-06-06 02:12:25 +0000

    build based on 14d4397

---
+++ previews/PR3/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API","page":"API","title":"API","text":"","category":"section"},{"location":"#HoldemCards.jl","page":"Home","title":"HoldemCards.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A package for Texas hold 'em cards.","category":"page"}]
+}
