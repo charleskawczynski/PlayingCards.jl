f646a0c8d9505c81c14a02b61d31ec4e70b6406f
Documenter.jl <documenter@juliadocs.github.io>
2023-08-04 09:26:16 +0000

    build based on 4b593b8

---
+++ previews/PR6/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API","page":"API","title":"API","text":"","category":"section"},{"location":"#PlayingCards.jl","page":"Home","title":"PlayingCards.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A package for representing playing cards for card games.","category":"page"}]
+}
